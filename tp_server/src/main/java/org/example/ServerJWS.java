6eb1459edbd7a55456b86109e7928e9fadfc2e2b
Yassine Essalmi <user@192.168.1.45>
2023-12-24 13:43:51 +0100

    second commit

--- tp_server/src/main/java/org/example/ServerJWS.java
+++ tp_server/src/main/java/org/example/ServerJWS.java
@@ -4,7 +4,7 @@ import jakarta.xml.ws.Endpoint;
 
 public class ServerJWS {
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8087/", new BanqueService());
-        System.out.println("web service deployed sur http://localhost:8087/");
+        Endpoint.publish("http://localhost:8088/", new BanqueService());
+        System.out.println("web service deployed sur http://localhost:8088/");
     }
 }
